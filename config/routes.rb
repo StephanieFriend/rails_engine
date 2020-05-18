f4d4c7f5c17a3d20d6f78cd22362298b92ae5395
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-17 22:27:58 -0600

    Add routes for resource traversal

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, except: [:new, :edit]
-      resources :merchants, except: [:new, :edit]
+      resources :items, except: [:new, :edit] do
+        resources :merchant, only: [:index], to: 'merchants#show'
+      end
+      resources :merchants, except: [:new, :edit] do
+        resources :items, only: [:index]
+      end
     end
   end
 end
