a2e8150cbb9a83c71df7cd2a4cca2735848e87b5
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-17 20:06:00 -0600

    Set up rake task

--- db/csv/sales_engine.rb
+++ db/csv/sales_engine.rb
@@ -2,30 +2,28 @@ require 'csv'
 require 'open-uri'
 
 class SalesEngine
-  class << self
   def csv_info
-      @csv_info ||= csv_locations.inject({}) do |new_csv_hash, (file_name, url)|
-        csv_text = open(url)
-        new_csv_hash[file_name] = CSV.parse(csv_text, headers: true, header_converters: :symbol)
-        new_csv_hash
-      end
+    @csv_info ||= csv_locations.inject({}) do |new_csv_hash, (file_name, url)|
+      csv_text = open(url)
+      new_csv_hash[file_name] = CSV.parse(csv_text, headers: true, header_converters: :symbol)
+      new_csv_hash
     end
+  end
 
-    def csv_locations
-      {
-        customers: original_location("customers"),
-        invoice_items: original_location("invoice_items"),
-        invoices: original_location("invoices"),
-        items: original_location("items"),
-        merchants: original_location("merchants"),
-        transactions: original_location("transactions")
-      }
-    end
+  def csv_locations
+    {
+      customers: original_location("customers"),
+      invoice_items: original_location("invoice_items"),
+      invoices: original_location("invoices"),
+      items: original_location("items"),
+      merchants: original_location("merchants"),
+      transactions: original_location("transactions")
+    }
+  end
 
-    private
+  private
 
-    def original_location(file_name)
-      "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/#{file_name}.csv"
-    end
+  def original_location(file_name)
+    "https://raw.githubusercontent.com/turingschool-examples/sales_engine/master/data/#{file_name}.csv"
   end
 end
