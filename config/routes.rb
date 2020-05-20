a40e96d3144e233d8bdc086771df2c8b8a3ad33f
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-19 23:07:13 -0600

    Update routes for find and find all endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :items, except: [:new, :edit] do
-        resources :merchant, only: [:index], to: 'merchants#show'
+      namespace :merchants do
+        get "/find_all", to: "search#index"
+        get "/find", to: "search#show"
       end
-      resources :merchants, except: [:new, :edit] do
-        resources :items, only: [:index]
+      namespace :items do
+        get "/find_all", to: "search#index"
+        get "/find", to: "search#show"
       end
+      resources :items, except: [:new, :edit]
+      resources :merchants, except: [:new, :edit]
+      get "/items/:item_id/merchant", to: "merchant_items#show"
+      get "/merchants/:merchant_id/items", to: "merchant_items#index"
     end
   end
 end
