87b0f02adc905280382f0d1220bba7c2c899110f
Stephanie Friend <stephaniefriend91@gmail.com>
2020-05-20 19:59:35 -0600

    Add active record logic for date endpoint

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,4 +4,10 @@ class Invoice < ApplicationRecord
   has_many :transactions, dependent: :destroy
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
+
+  def self.total_revenue(start_date, end_date)
+    joins(:invoice_items, :transactions).where(transactions: {result: :success})
+        .where(created_at: Date.parse(start_date).beginning_of_day...Date.parse(end_date).end_of_day)
+        .sum('invoice_items.quantity * invoice_items.unit_price')
+  end
 end
